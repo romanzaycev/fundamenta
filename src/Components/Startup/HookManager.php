78fb09afc33a0914b2e5011498f91763827958e9
Roman Zaycev <box@romanzaycev.ru>
2025-07-03 19:46:37 +0700

    `server_started` hook

--- src/Components/Startup/HookManager.php
+++ src/Components/Startup/HookManager.php
@@ -9,6 +9,7 @@ interface HookManager
     public const ON_REQUEST = "on_request";
     public const ON_SESSION_STARTED = "on_session_started";
     public const ON_REQUEST_TERMINATED = "on_request_terminated";
+    public const ON_SERVER_STARTED = "on_server_started";
 
     public function add(string $hook, callable $handler): void;
     public function remove(string $hook, callable $handler): void;
